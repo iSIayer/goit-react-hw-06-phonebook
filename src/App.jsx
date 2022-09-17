c7e4c7d87b085ddb89a8161c5c1ddb3e3f2aad4b
Ihor Kolontaievskiy <gamerspeed86@gmail.com>
2022-09-17 03:10:28 +0300

    add defaultList to App

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,6 @@ import { Form } from './components/Form/Form';
 import { Filter } from './components/Filter/Filter';
 import { ContactsList } from './components/Contacts/ContactsList';
 
-const defaultList = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
-
 export function App() {
   return (
     <Container>
